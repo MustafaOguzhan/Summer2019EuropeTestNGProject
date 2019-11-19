a485e12f3d51fb140225eb5fa238be0f21b6fda0
Mustafa <oguzhan_onal@hotmail.com>
2019-11-19 13:08:49 +0100

    day7 DisabledElement

--- src/test/java/com/cybertek/tests/day7_types_of_elements/DisabledElementTest.java
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/DisabledElementTest.java
@@ -1,13 +1,42 @@
 package com.cybertek.tests.day7_types_of_elements;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class DisabledElementTest {
 
+    @Test
+        public void test1(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/radio_buttons" );
 
-    WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebElement greenRadioButton = driver.findElement(By.id("green"));
+        System.out.println("Is green button enabled: "+greenRadioButton.isEnabled());
+
+        greenRadioButton.click();
+
+    }
+    @Test
+    public void test2(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/dynamic_controls" );
+
+        WebElement inputBox = driver.findElement(By.cssSelector("#input-example>input"));
+
+        System.out.println("Is input enabled: "+ inputBox.isEnabled());
+        inputBox.sendKeys("Mike Smith");
+
+
+
+
+
+
+    }
 
 
 
